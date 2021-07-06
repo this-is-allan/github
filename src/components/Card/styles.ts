290eeccacb57fef5519ef1c1a7bdfa20563af9cf
Allan Carlos <allancarlosdeveloper@gmail.com>
2021-07-06 12:42:10 -0300

    style(card): improvements in shadow

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -3,12 +3,13 @@ import styled, { css } from "styled-components";
 const CardZoomEffect = css`
   &:hover {
     transform: scale(1.03, 1.03);
+    
     &:after {
       opacity: 1;
       transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
     }
   }
-
+  
   &:after {
     content: "";
     border-radius: .8rem;
@@ -18,19 +19,20 @@ const CardZoomEffect = css`
     left: 0;
     width: 100%;
     height: 100%;
-    box-shadow: 0 1px 5px 0 rgb(192, 208, 230, 0.8);
+    box-shadow: 0 2px 3px 0 rgb(192, 208, 230, 0.8);
     opacity: 0;
     transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
   }
-`;
+  `;
 
 export const CardContainer = styled.div`
   position: relative;
   background-color: #fff;
   border-radius: .8rem;
   cursor: pointer;
-  box-shadow: 0 1px 4px 0 rgb(192, 208, 230, 0.8);
+  box-shadow: 0 1px 1px 0 rgb(192, 208, 230, 0.8);
   transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  text-decoration: none;
 
   ${CardZoomEffect}
 
