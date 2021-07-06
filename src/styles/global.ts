af66140bb6aaabd49ebba75277dc007a25944b85
Allan Carlos <allancarlosdeveloper@gmail.com>
2021-07-06 06:50:05 -0300

    feat: configure user list grid

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,5 +13,13 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     font-family: ${({ theme }) => theme.font.family};
+    color: ${({ theme }) => theme.font.color};
+    background: ${({ theme }) => theme.body};
+  }
+
+  .users-list-grid {
+    display: grid;
+    grid-gap: 2.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   }
 `;
