d88e11dfd89daf126a0d6ca7307d3406321f23f7
Allan Carlos <allancarlosdeveloper@gmail.com>
2021-07-06 23:39:26 -0300

    feat: loading in the home page

--- src/organisms/UserList/index.tsx
+++ src/organisms/UserList/index.tsx
@@ -2,34 +2,45 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 
 import Card from "../../components/Card";
+import Loading from "../../components/Loading";
 
 import { User } from "../../utils/types";
 
 const UserList = () => {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState<User[]>([]);
   
   useEffect(() => {
     const fetchUsers = async () => {
+      setLoading(true)
       const response = await fetch('https://api.github.com/users')
-        .then((response) => response.json())
-    
+      .then((response) => response.json())
+      
       setUsers(response)
+      setLoading(false)
     }
-
+    
     fetchUsers()
   }, [])
   
   return (
     <div className="users-list-grid">
-      {users.map((user) => (
-        <Link to={`users/${user.login}`}>
-          <Card
-            key={user.id}
-            image={user.avatar_url}
-            title={user.login}
-          />
-        </Link>
-      ))}
+      {loading ?
+        <Loading />
+      : (
+        <>
+          {users.map((user) => (
+            <Link to={`users/${user.login}`}>
+              <Card
+                key={user.id}
+                image={user.avatar_url}
+                title={user.login}
+              />
+            </Link>
+          ))}
+        </>
+      )
+      }
     </div>
   )
 }
